d4558245cb29145890c7bb8f8e5cae2b8162839c
gabu <denis.afonso@gmail.com>
2013-11-13 16:51:53 -0500

    Installed libv8 gem

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,13 +1,15 @@
-set :stage, :staging
+set :stage, :development
+set :rails_env, 'development'
+
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{denisra.webfactional.com}
+role :web, %w{denisra.webfactional.com}
+role :db,  %w{denisra.webfactional.com}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +17,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'denisra.webfactional.com', user: 'denisra', roles: %w{web app db} #, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
